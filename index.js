38cf99b91d4d6dca683e36cb382273a50981097c
fanx <faridazhari46@gmail.com>
2025-08-13 20:34:55 +0700

    add accordion logic

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const listButton = document.querySelectorAll("#list button");
 
 listButton.forEach((btn) => {
   btn.addEventListener("click", () => {
+    document.querySelectorAll("#list p").forEach((p) => (p.hidden = true));
+    document
+      .querySelectorAll("#list button img")
+      .forEach((img) => (img.src = "./assets/images/icon-plus.svg"));
+
     const para = btn.closest("li").querySelector("p");
     const img = btn.closest("li").querySelector("img");
     para.hidden
