64c27722d1ebcbeb0bfd0f70ccfd02a1f70fd64c
fanx <faridazhari46@gmail.com>
2025-08-13 21:46:31 +0700

    fix accordion logic

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const listButton = document.querySelectorAll("#list button");
+const listButtons = document.querySelectorAll("#list button");
 
-listButton.forEach((btn) => {
+listButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
+    const para = btn.closest("li").querySelector("p");
+    const img = btn.querySelector("img");
+
+    const isHidden = para.hidden; // check first
+
+    // close all
     document.querySelectorAll("#list p").forEach((p) => (p.hidden = true));
     document
       .querySelectorAll("#list button img")
-      .forEach((img) => (img.src = "./assets/images/icon-plus.svg"));
+      .forEach((i) => (i.src = "./assets/images/icon-plus.svg"));
 
-    const para = btn.closest("li").querySelector("p");
-    const img = btn.closest("li").querySelector("img");
-    para.hidden
-      ? ((para.hidden = false),
-        img.setAttribute("src", "./assets/images/icon-minus.svg"))
-      : ((para.hidden = true),
-        img.setAttribute("src", "./assets/images/icon-plus.svg"));
+    // if it was hidden before, open it now
+    if (isHidden) {
+      para.hidden = false;
+      img.src = "./assets/images/icon-minus.svg";
+    }
   });
 });
